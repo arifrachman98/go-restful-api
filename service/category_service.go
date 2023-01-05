895fb60cb0cf193917a49b29f66dab2b0527e56f
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-05 13:06:33 +0700

    Update category_service.go

--- service/category_service.go
+++ service/category_service.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+
+	"github.com/arifrachman98/go-restful-api/model/web"
+)
+
+type CategoryService interface {
+	Create(c context.Context, r web.CategoryCreateRequest) web.CategoryResponse
+	Update(c context.Context, r web.CategoryUpdateRequest) web.CategoryResponse
+	Delete(c context.Context, categoryID int)
+	FindByID(c context.Context, categoryID int) web.CategoryResponse
+	FindAll(c context.Context) []web.CategoryResponse
+}
