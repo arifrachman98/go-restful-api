74facb14699894b580d27ad1fb046bee4bc222ff
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-05 18:21:58 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"github.com/arifrachman98/go-restful-api/app"
+	"github.com/arifrachman98/go-restful-api/controller"
+	"github.com/arifrachman98/go-restful-api/repository"
+	"github.com/arifrachman98/go-restful-api/service"
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+
+	cRepos := repository.NewCategoryRepository()
+	cService := service.NewCategoryService(cRepos, db, validate)
+	cController := controller.NewCategoryController(cService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", cController.FindAll)
+	router.GET("/api/categories/:categoryId", cController.FindById)
+	router.POST("/api/categories", cController.Create)
+	router.PUT("/api/categories/:categoryId", cController.Update)
+	router.DELETE("/api/categories/:categoryId", cController.Delete)
+
+}
