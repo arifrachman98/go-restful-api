e3963cc5b99d375c2c9f74f8cfc2e94463244f47
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-05 16:34:22 +0700

    Add new file category_controller_impl.go

---
+++ controller/category_controller_impl.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type CategoryControllerImpl struct {
+}
+
+func (c *CategoryControllerImpl) Create(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
+
+func (c *CategoryControllerImpl) Update(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
+
+func (c *CategoryControllerImpl) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
+
+func (c *CategoryControllerImpl) FindById(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
+
+func (c *CategoryControllerImpl) FindAll(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
