e2139307ba22d9f2b7067d513851101db8853c24
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-05 11:46:41 +0700

    Update category_repository.go

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/arifrachman98/go-restful-api/model/domain"
+)
+
+type CategoryRepository interface {
+	Save(c context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Update(c context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Delete(c context.Context, tx sql.Tx, category domain.Category)
+	FindById(c context.Context, tx sql.Tx, categoryID int) domain.Category
+	FindAll(c context.Context, tx sql.Tx) []domain.Category
+}
