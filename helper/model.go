026bf9914a9581a49ae5f1961add0d77d51f6c7a
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-05 13:31:37 +0700

    Update model.go

--- helper/model.go
+++ helper/model.go
@@ -1,1 +1,13 @@
 package helper
+
+import (
+	"github.com/arifrachman98/go-restful-api/model/domain"
+	"github.com/arifrachman98/go-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
