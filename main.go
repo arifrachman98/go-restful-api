697ae3e96d7bfc5c1904c7f651391739df472824
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-10 23:57:09 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/arifrachman98/go-restful-api/app"
 	"github.com/arifrachman98/go-restful-api/controller"
+	"github.com/arifrachman98/go-restful-api/helper"
 	"github.com/arifrachman98/go-restful-api/repository"
 	"github.com/arifrachman98/go-restful-api/service"
 	"github.com/go-playground/validator/v10"
@@ -26,4 +31,11 @@ func main() {
 	router.PUT("/api/categories/:categoryId", cController.Update)
 	router.DELETE("/api/categories/:categoryId", cController.Delete)
 
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicHelper(err)
 }
