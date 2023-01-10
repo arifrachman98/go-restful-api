4ed879b5ced9bb8823c3a6eb5bb163d8ee995b17
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-11 00:04:00 +0700

    Add new directory and new file

---
+++ exception/error_handler.go
@@ -0,0 +1,72 @@
+package exception
+
+import (
+	"net/http"
+
+	"github.com/arifrachman98/go-restful-api/helper"
+	"github.com/arifrachman98/go-restful-api/model/web"
+	"github.com/go-playground/validator/v10"
+)
+
+func internalServerError(w http.ResponseWriter, r *http.Request, err interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+
+	webResponse := web.WebResponse{
+		Code:   http.StatusInternalServerError,
+		Status: "Internal Server Error",
+		Data:   err,
+	}
+	helper.WriteToResponseBody(w, webResponse)
+}
+
+func validationErrors(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func notFoundError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(NotFoundError)
+	if ok {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "Not Found",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
+
+	if notFoundError(w, r, err) {
+		return
+	}
+
+	if validationErrors(w, r, err) {
+		return
+	}
+
+	internalServerError(w, r, err)
+}
