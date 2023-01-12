c750a22b9e127ed0b4402325b6592138a3d749b5
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-12 18:50:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/arifrachman98/go-restful-api/repository"
 	"github.com/arifrachman98/go-restful-api/service"
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -23,13 +22,7 @@ func main() {
 	cService := service.NewCategoryService(cRepos, db, validate)
 	cController := controller.NewCategoryController(cService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", cController.FindAll)
-	router.GET("/api/categories/:categoryId", cController.FindById)
-	router.POST("/api/categories", cController.Create)
-	router.PUT("/api/categories/:categoryId", cController.Update)
-	router.DELETE("/api/categories/:categoryId", cController.Delete)
+	router := app.NewRouter(cController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
