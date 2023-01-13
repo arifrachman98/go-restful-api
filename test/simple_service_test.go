12f6cde1516ac4dc186eb8645662503cda3d4bee
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-13 17:32:44 +0700

    Update simple_service_test.go

--- test/simple_service_test.go
+++ test/simple_service_test.go
@@ -5,10 +5,23 @@ import (
 	"testing"
 
 	"github.com/arifrachman98/go-restful-api/simple"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleService(t *testing.T) {
-	simpleService, err := simple.InitializedService()
+	simpleService, err := simple.InitializedService(true)
 	fmt.Println(err)
 	fmt.Println(simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpServ, err := simple.InitializedService(false)
+	assert.Nil(t, err)
+	assert.NotNil(t, simpServ)
+}
+
+func TestSimpleServiceError(t *testing.T) {
+	simpService, err := simple.InitializedService(true)
+	assert.NotNil(t, err)
+	assert.Nil(t, simpService)
 }
