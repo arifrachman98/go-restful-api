46205d029ce11c471ab1ae9e338e8ccf65b73f08
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-13 17:50:59 +0700

    Update simple.go

--- simple/simple.go
+++ simple/simple.go
@@ -18,7 +18,7 @@ func NewSimpleRepository(isError bool) *SimpleRepository {
 
 func NewSimpleService(repository *SimpleRepository) (*SimpleService, error) {
 	if repository.Error {
-		return nil, errors.New("Failed create service")
+		return nil, errors.New("failed create service")
 	} else {
 		return &SimpleService{
 			SimpleRepository: repository,
