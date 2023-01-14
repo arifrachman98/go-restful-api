8e0a9f31fc51004ea2905bf52eb6bebb02886e4c
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-14 21:12:09 +0700

    Update file.go

--- simple/file.go
+++ simple/file.go
@@ -1,1 +1,20 @@
 package simple
+
+import "fmt"
+
+type File struct {
+	Name string
+}
+
+func (f File) Close() {
+	fmt.Println("Close File", f.Name)
+}
+
+func NewFile(name string) (*File, func()) {
+	files := &File{
+		Name: name,
+	}
+	return files, func() {
+		files.Close()
+	}
+}
