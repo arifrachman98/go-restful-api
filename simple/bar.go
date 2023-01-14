9c0584c647b5ab8d2d81c88c80a582f1c4d2ffc7
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-14 19:55:58 +0700

    Add new file bar.go

---
+++ simple/bar.go
@@ -0,0 +1,18 @@
+package simple
+
+type BarRepository struct {
+}
+
+type BarService struct {
+	*BarRepository
+}
+
+func NewBarRepository() *BarRepository {
+	return &BarRepository{}
+}
+
+func NewBarService(barRepository *BarRepository) *BarService {
+	return &BarService{
+		BarRepository: barRepository,
+	}
+}
