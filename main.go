4057a0950ef7b48a3c8437369e49b4274d82a5e2
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-14 22:06:09 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,35 +3,21 @@ package main
 import (
 	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/arifrachman98/go-restful-api/app"
-	"github.com/arifrachman98/go-restful-api/controller"
-	"github.com/arifrachman98/go-restful-api/exception"
 	"github.com/arifrachman98/go-restful-api/helper"
 	"github.com/arifrachman98/go-restful-api/middleware"
-	"github.com/arifrachman98/go-restful-api/repository"
-	"github.com/arifrachman98/go-restful-api/service"
-	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func NewServer(AuthMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
+		Addr:    "localhost:3000",
+		Handler: AuthMiddleware,
+	}
+}
 
 func main() {
 
-	db := app.NewDB()
-	validate := validator.New()
-
-	cRepos := repository.NewCategoryRepository()
-	cService := service.NewCategoryService(cRepos, db, validate)
-	cController := controller.NewCategoryController(cService)
-
-	router := app.NewRouter(cController)
-	router.PanicHandler = exception.ErrorHandler
-
-	server := http.Server{
-		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
-	}
-
+	server := InitServer()
 	err := server.ListenAndServe()
 	helper.PanicHelper(err)
 }
