8d18bbea27488e3637ea0520e417d30bc3a47b5a
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-14 22:05:11 +0700

    Update injector.go main file

--- injector.go
+++ injector.go
@@ -1,11 +1,35 @@
 //go:build wireinject
 // +build wireinject
+
 package main
 
 import (
+	"github.com/arifrachman98/go-restful-api/app"
+	"github.com/arifrachman98/go-restful-api/controller"
+	"github.com/arifrachman98/go-restful-api/middleware"
+	"github.com/arifrachman98/go-restful-api/repository"
+	"github.com/arifrachman98/go-restful-api/service"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func () {
-	
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	wire.Bind(new(repository.CategoryRepository), new(*repository.CategoryRepositoryImpl)),
+	service.NewCategoryService,
+	wire.Bind(new(service.CategoryService), new(*service.CategoryServiceImpl)),
+	controller.NewCategoryController,
+	wire.Bind(new(controller.CategoryController), new(*controller.CategoryControllerImpl)),
+)
+
+func InitServer() *http.Server {
+	wire.Build(app.NewDB, validator.New, categorySet, app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+
+	return nil
 }
